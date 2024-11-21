ba0ffd704fb954d86125c14ef586a9040342dca6
Sam R <richardsonsam123@gmail.com>
2024-11-21 17:40:47 +1300

    Basic UI, ready for crawling

--- testscraper.py
+++ testscraper.py
@@ -1,20 +1,30 @@
-import tkinter
+import asyncio
+import tkinter as tk
+from tkinter import *
 import requests
-
-#UI
-top = tkinter.Tk()
-
-
-
-
 
 def scrape(url: str) -> None:
     page = requests.get(url)
     print(page.text)
 
-async def main():
-    print("test")
+def Take_input(inputtxt):
+    INPUT = inputtxt.get("1.0", "end-1c")
+    print(INPUT)
     
+def showUI():
+    root = tk.Tk()
+    root.geometry("800x200")
+    root.title("PhilosoLinker")
+    inputtxt = Text(root, height = 1, width = 60)
+    button = Button(root, height = 2, width = 20, text="Crawl", command = lambda:Take_input(inputtxt))
+
+    inputtxt.pack()
+    button.pack()
+    
+    root.mainloop()
+    
+async def main():
+    showUI()
     
     
 if __name__ == "__main__":
