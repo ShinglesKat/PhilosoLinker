81239c4fdbf4fc248f44117d6aa0abad77924d17
Sam R <richardsonsam123@gmail.com>
2024-11-26 01:41:01 +1300

    basic link grabbing

--- testscraper.py
+++ testscraper.py
@@ -1,25 +1,47 @@
 import asyncio
 import tkinter as tk
+from bs4 import BeautifulSoup
 from tkinter import *
 import requests
+import re
 
+
+wikipedia_link_format = r"/wiki/[A-Za-z0.9_]+"
 def scrape(url: str) -> None:
     page = requests.get(url)
-    print(page.text)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    #Find all links in website
+    website_links = soup.find_all('a')
+    wikipedia_links = []
+    for link in website_links:
+        href = link.get('href')
+        if href and re.search(wikipedia_link_format, href):
+            wikipedia_links.append(href)
+        
 
-def Take_input(inputtxt):
-    INPUT = inputtxt.get("1.0", "end-1c")
+    for link_to_check in wikipedia_links:
+        print(link_to_check)
+
+def paste_link(input_txt):
+    input_txt.insert("1.0", "https://en.wikipedia.org/wiki/Old_School_RuneScape")
+    
+def take_input(input_txt):
+    INPUT = input_txt.get("1.0", "end-1c")
+    scrape(INPUT)
     print(INPUT)
     
 def showUI():
     root = tk.Tk()
     root.geometry("800x200")
     root.title("PhilosoLinker")
-    inputtxt = Text(root, height = 1, width = 60)
-    button = Button(root, height = 2, width = 20, text="Crawl", command = lambda:Take_input(inputtxt))
+    input_txt = Text(root, height = 1, width = 60)
+    button = Button(root, height = 2, width = 20, text="Crawl", command = lambda:take_input(input_txt))
+    paste_osrs_link = Button(root, height = 1, width = 10, text="Link", command = lambda:paste_link(input_txt))
 
-    inputtxt.pack()
+    input_txt.pack()
     button.pack()
+    paste_osrs_link.pack()
     
     root.mainloop()
     
